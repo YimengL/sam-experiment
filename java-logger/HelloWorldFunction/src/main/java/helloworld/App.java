93b116edaada4d4c03352d1ca9d82280c62d1494
YimengL <yimengcsu@gmail.com>
2020-07-19 20:44:36 -0500

    update logger

--- java-logger/HelloWorldFunction/src/main/java/helloworld/App.java
+++ java-logger/HelloWorldFunction/src/main/java/helloworld/App.java
@@ -16,11 +16,11 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
     private static final Logger logger = LogManager.getLogger(App.class);
 
     public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
-        logger.error("[Yimeng]ERROR log");
-        logger.warn("[Yimeng]WARN log");
-        logger.info("[Yimeng]INFO log");
-        logger.debug("[Yimeng]DEBUG log");
-        logger.trace("[Yimeng]TRACE log");
+        logger.error("[App]ERROR log");
+        logger.warn("[App]WARN log");
+        logger.info("[App]INFO log");
+        logger.debug("[App]DEBUG log");
+        logger.trace("[App]TRACE log");
 
         logger.warn("Log level: " + System.getenv("LOG_LEVEL"));
 
