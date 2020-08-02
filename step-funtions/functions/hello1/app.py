a277f06b7216bbf2fe04f7bbd05482f3e71ccdd8
YimengL <yimengcsu@gmail.com>
2020-08-02 00:09:29 -0500

    update 2 lambda functions

--- step-funtions/functions/hello1/app.py
+++ step-funtions/functions/hello1/app.py
@@ -1,16 +1,39 @@
 import logging
 import datetime
+import boto3
+from botocore.exceptions import ClientError
+
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
+s3_client = boto3.client('s3')
+
 
 def lambda_handler(event, context):
     logger.info("hello1 START")
     logger.info(event)
 
+    current_time = str(datetime.datetime.now())
+
+    key = f'partition-{current_time}.json'
+    success = upload_file(key)
+
     response = {}
-    response['type'] =  event.get('type')
-    response['timestamp'] = str(datetime.datetime.now())
+    response['update'] = True if success else False
+    response['type'] =  event.get('type', 'default-type')
+    response['timestamp'] = current_time
     response['message'] = 'Hello World'
+    response['key'] = key
+
+    logger.info(response)
 
     return response
+
+def upload_file(key):
+    try:
+        s3_client.upload_file("partition.json", "local-test-demo-bucket", key)
+        return True
+    except ClientError as e:
+        logger.error(e)
+        return False
--- step-funtions/functions/hello2/app.py
+++ step-funtions/functions/hello2/app.py
@@ -1,12 +1,23 @@
 import logging
 import datetime
+import json
+import boto3
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+BUCKET = "local-test-demo-bucket"
+
+client = boto3.client('s3')
+
 def lambda_handler(event, context):
     logger.info("hello2 START")
     logger.info(event)
+    
+    json_data = json.loads(client.get_object(Bucket=BUCKET, Key=event.get('key'))['Body'].read())
+    logger.info("[END] loading data")
+
+    logger.info(json_data.keys())
 
     response = {}
 
